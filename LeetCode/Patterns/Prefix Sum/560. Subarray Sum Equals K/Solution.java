ce23f11d3e3854728e7bb5ee55147e53fa1cfb18
Bhagyalakshmi217 <bhagyalakshmi2716@gmail.com>
2026-08-13 10:31:38 +0530

    Add Java Solution to Patterns/Prefix Sum

--- LeetCode/Patterns/Prefix Sum/560. Subarray Sum Equals K/Solution.java
+++ LeetCode/Patterns/Prefix Sum/560. Subarray Sum Equals K/Solution.java
@@ -15,26 +15,43 @@
 //     return count;
 // }
 // }
-class Solution {
-    public int subarraySum(int[] nums, int k) {
-int sum = 0;
-int count = 0;
+// class Solution {
+//     public int subarraySum(int[] nums, int k) {
+// int sum = 0;
+// int count = 0;
 
-HashMap<Integer, Integer> map = new HashMap<>();
+// HashMap<Integer, Integer> map = new HashMap<>();
 
-map.put(0, 1);
+// map.put(0, 1);
 
-for (int num : nums) {
+// for (int num : nums) {
 
-    sum += num;
+//     sum += num;
 
-    if (map.containsKey(sum - k)) {
-        count += map.get(sum - k);
-    }
+//     if (map.containsKey(sum - k)) {
+//         count += map.get(sum - k);
+//     }
 
-    map.put(sum, map.getOrDefault(sum, 0) + 1);
-}
+//     map.put(sum, map.getOrDefault(sum, 0) + 1);
+// }
 
-return count;
+// return count;
+//     }
+// }
+
+class Solution{
+    public int subarraySum(int[] nums,int k){
+        int sum=0;
+        int count=0;
+        HashMap<Integer,Integer> map=new HashMap<>();
+        map.put(0,1);
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
+            if(map.containsKey(sum-k)){
+                count+=map.get(sum-k);
+            }
+            map.put(sum,map.getOrDefault(sum,0)+1);
+        }
+        return count;
     }
 }

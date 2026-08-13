50cd005400c0fab0cfd0cdd7c17d6ba5f04b948e
Bhagyalakshmi217 <bhagyalakshmi2716@gmail.com>
2026-08-13 22:14:18 +0530

    Add Java Solution to Patterns/Prefix Sum

---
+++ LeetCode/Patterns/Prefix Sum/523. Continuous Subarray Sum/Solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean checkSubarraySum(int[] nums, int k) {
+        int sum=0;
+        HashMap<Integer,Integer> map=new HashMap<>();
+        map.put(0,-1);
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
+            int rem=((sum%k)+k)%k;
+            if(map.containsKey(rem)){
+                return true;
+
+            }else{
+                map.put(rem,map.getOrDefault(rem,0)+1);
+            }
+
+        }
+        return false;
+
+        
+    }
+}

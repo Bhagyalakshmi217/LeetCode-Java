50cf737fcfeddc089b08a1f58eb49e79726e35ca
Bhagyalakshmi217 <bhagyalakshmi2716@gmail.com>
2026-08-14 21:44:53 +0530

    Add Medium Java Solution from LeetCode

--- LeetCode/Java/Medium/739. Daily Temperatures/Solution.java
+++ LeetCode/Java/Medium/739. Daily Temperatures/Solution.java
@@ -1,18 +1,34 @@
+// class Solution {
+//     public int[] dailyTemperatures(int[] temperatures) {
+//         int n=temperatures.length;
+//         int ans[]=new int[n];
+//         Stack<Integer> stack=new Stack();
+//         for(int i=0;i<n;i++){
+//             while(!stack.isEmpty() && temperatures[i]>temperatures[stack.peek()]){
+//                 int prevIndex=stack.pop();
+//                 ans[prevIndex]=i-prevIndex;
+
+//             }
+//             stack.push(i);
+
+//         }
+//         return ans;
+        
+//     }
+// }
+
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
-        int n=temperatures.length;
-        int ans[]=new int[n];
-        Stack<Integer> stack=new Stack();
-        for(int i=0;i<n;i++){
+        Deque<Integer> stack=new ArrayDeque<>();
+        int[] result=new int[temperatures.length];
+        Arrays.fill(result,0);
+        for(int i=0;i<temperatures.length;i++){
             while(!stack.isEmpty() && temperatures[i]>temperatures[stack.peek()]){
-                int prevIndex=stack.pop();
-                ans[prevIndex]=i-prevIndex;
-
+            int index=stack.pop();
+            result[index]=i-index;
             }
             stack.push(i);
-
         }
-        return ans;
-        
+        return result;
     }
 }

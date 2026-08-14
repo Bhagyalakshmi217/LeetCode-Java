bd0876caa3fc8476dbf79dbcff7c0321d9a6c48c
Bhagyalakshmi217 <bhagyalakshmi2716@gmail.com>
2026-08-14 14:45:00 +0530

    Add Java Solution to Patterns/Stack & Queue

--- LeetCode/Patterns/Stack & Queue/155. Min Stack/Solution.java
+++ LeetCode/Patterns/Stack & Queue/155. Min Stack/Solution.java
@@ -10,12 +10,11 @@ class MinStack {
     
     public void push(int val) {
         stack.push(val);
-        if(minStack.isEmpty() || val<=minStack.peek()){
+        if(minStack.isEmpty()){
             minStack.push(val);
+        }else{
+            minStack.push(Math.min(val,minStack.peek()));
         }
-        // }else{
-        //     minStack.push(Math.min(val,minStack.peek()));
-        // }
         
     }
     
@@ -36,3 +35,11 @@ class MinStack {
     }
 }
 
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(value);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */

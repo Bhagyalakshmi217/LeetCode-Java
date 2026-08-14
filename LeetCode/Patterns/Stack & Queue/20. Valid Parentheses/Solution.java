34623920bc9ea57882ffe88faf6fc9c44a6b9ae4
Bhagyalakshmi217 <bhagyalakshmi2716@gmail.com>
2026-08-14 11:36:58 +0530

    Add Java Solution to Patterns/Stack & Queue

--- LeetCode/Patterns/Stack & Queue/20. Valid Parentheses/Solution.java
+++ LeetCode/Patterns/Stack & Queue/20. Valid Parentheses/Solution.java
@@ -1,22 +1,42 @@
-class Solution {
-    public boolean isValid(String s) {
-        Stack<Character> stack=new Stack();
+// class Solution {
+//     public boolean isValid(String s) {
+//         Stack<Character> stack=new Stack();
+//         for(char ch:s.toCharArray()){
+//             if(ch=='(' || ch=='[' || ch=='{'){
+//                 stack.push(ch);
+//             }
+//             else{
+//                 if(stack.isEmpty()){
+//                     return false;
+//                 }
+//                 char top=stack.pop();
+
+//                 if((ch==')' && top!='(') || (ch==']' && top!='[') || (ch=='}' && top!='{')){
+//                     return false;
+//                 }
+//             }
+//         }
+//         return stack.isEmpty();
+        
+//     }
+// }
+
+class Solution{
+    public boolean isValid(String s){
+        Deque<Character> stack=new ArrayDeque<>();
         for(char ch:s.toCharArray()){
             if(ch=='(' || ch=='[' || ch=='{'){
                 stack.push(ch);
-            }
-            else{
+            }else{
                 if(stack.isEmpty()){
                     return false;
                 }
                 char top=stack.pop();
-
                 if((ch==')' && top!='(') || (ch==']' && top!='[') || (ch=='}' && top!='{')){
                     return false;
                 }
             }
         }
         return stack.isEmpty();
-        
     }
 }

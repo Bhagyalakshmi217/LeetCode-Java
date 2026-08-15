5d1d30d5435108d64ff8d9e00338783080d4ef5a
Bhagyalakshmi217 <bhagyalakshmi2716@gmail.com>
2026-08-15 19:35:06 +0530

    Add Medium Java Solution from LeetCode

---
+++ LeetCode/Java/Medium/937. Online Stock Span/Solution.java
@@ -0,0 +1,19 @@
+class StockSpanner {
+    Deque<int[]> stack;
+
+    public StockSpanner() {
+        stack=new ArrayDeque<>();   
+    }    
+    public int next(int price) {
+        int span=1;
+        while(!stack.isEmpty() && price>=stack.peek()[0]){
+            int[] prev=stack.pop();
+            span+=prev[1];
+            }
+        stack.push(new int[]{price,span});
+        
+        return span;
+        
+    }
+}
+

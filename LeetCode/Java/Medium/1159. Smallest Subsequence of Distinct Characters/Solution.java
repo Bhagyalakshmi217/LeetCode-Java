b075247839a5360badce179a844214f209bd9103
Bhagyalakshmi217 <bhagyalakshmi2716@gmail.com>
2026-08-17 10:21:49 +0530

    Add Medium Java Solution from LeetCode

---
+++ LeetCode/Java/Medium/1159. Smallest Subsequence of Distinct Characters/Solution.java
@@ -0,0 +1,32 @@
+class Solution {
+    public String smallestSubsequence(String s) {
+        Deque<Character> stack=new ArrayDeque<>();
+        boolean[] used=new boolean[26];
+        int[] count=new int[26];
+        for(int i=0;i<s.length();i++){
+            count[s.charAt(i)-'a']++;
+        }
+        for(int i=0;i<s.length();i++){
+            char current=s.charAt(i);
+            count[current-'a']--;
+            if(used[current-'a']){
+                continue;
+            }
+            while(!stack.isEmpty() && stack.peek()>current && count[stack.peek()-'a']>0){
+                char top=stack.pop();                
+                used[top-'a']=false;
+            }
+            
+            stack.push(current);
+            used[current-'a']=true;
+        }
+        StringBuilder result=new StringBuilder();
+        while(!stack.isEmpty()){
+            result.append(stack.removeLast());
+        }
+        return result.toString();
+
+        
+        
+    }
+}

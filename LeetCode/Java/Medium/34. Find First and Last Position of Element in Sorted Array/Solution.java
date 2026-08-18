1e0aa2bbd823e562f58b8c426e67e8e38fffc445
Bhagyalakshmi217 <bhagyalakshmi2716@gmail.com>
2026-08-18 19:14:58 +0530

    Add Medium Java Solution from LeetCode

--- LeetCode/Java/Medium/34. Find First and Last Position of Element in Sorted Array/Solution.java
+++ LeetCode/Java/Medium/34. Find First and Last Position of Element in Sorted Array/Solution.java
@@ -1,28 +1,58 @@
-class Solution {
-    public int[] searchRange(int[] nums, int target) {
-        int first=find(nums,target,true);
-        int last=find(nums,target,false);
-        return new int[]{first,last};
+// class Solution {
+//     public int[] searchRange(int[] nums, int target) {
+//         int first=find(nums,target,true);
+//         int last=find(nums,target,false);
+//         return new int[]{first,last};
 
         
+//     }
+//     int find(int nums[],int target,boolean firstOccurence){
+//         int left=0;
+//         int right=nums.length-1;
+//         int mid;
+//         int answer=-1;
+
+//         while(left<=right){
+//             mid=left+(right-left)/2;
+
+//             if(nums[mid]==target){
+//                 answer=mid;
+
+//                 if(firstOccurence){
+//                     right=mid-1;
+//                 }else{
+//                     left=mid+1;
+//                 }
+//             }
+//             else if(nums[mid]<target){
+//                 left=mid+1;
+//             }
+//             else{
+//                 right=mid-1;
+//             }
+
+
+//         }
+//         return answer;
+//     }
+// }
+
+
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int first=findFirst(nums,target);
+        int last=findLast(nums,target);
+        return new int[]{first,last};
     }
-    int find(int nums[],int target,boolean firstOccurence){
+    int findFirst(int[] nums,int target){
         int left=0;
         int right=nums.length-1;
-        int mid;
         int answer=-1;
-
         while(left<=right){
-            mid=left+(right-left)/2;
-
+            int mid=left+(right-left)/2;
             if(nums[mid]==target){
                 answer=mid;
-
-                if(firstOccurence){
-                    right=mid-1;
-                }else{
-                    left=mid+1;
-                }
+                right=mid-1;
             }
             else if(nums[mid]<target){
                 left=mid+1;
@@ -30,9 +60,26 @@ class Solution {
             else{
                 right=mid-1;
             }
-
-
         }
         return answer;
     }
+    int findLast(int[] nums,int target){
+        int left=0;int right=nums.length-1;
+        int answer=-1;
+        while(left<=right){
+            int mid=left+(right-left)/2;
+            if(nums[mid]==target){
+                answer=mid;
+                left=mid+1;
+            }
+            else if(nums[mid]<target){
+                left=mid+1;
+            }
+            else{
+                right=mid-1;
+            }
+        }
+        return answer;
+    }
+    
 }

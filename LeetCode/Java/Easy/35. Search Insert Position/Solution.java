259d9af491e484783832e78b35e9ac697a8f5b5c
Bhagyalakshmi217 <bhagyalakshmi2716@gmail.com>
2026-08-20 21:54:12 +0530

    Add Easy Java Solution from LeetCode

--- LeetCode/Java/Easy/35. Search Insert Position/Solution.java
+++ LeetCode/Java/Easy/35. Search Insert Position/Solution.java
@@ -6,15 +6,13 @@ class Solution {
             int mid=left+(right-left)/2;
             if(nums[mid]==target){
                 return mid;
-            }
-            else if(nums[mid]<target){
+            }else if(nums[mid]<target){
                 left=mid+1;
-            }
-            else{
+            }else{
                 right=mid-1;
             }
         }
-        return right+1;
+        return left;
         
     }
 }

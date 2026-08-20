a17d1a0b0b97b95340cafc9efbb6eb40525bef7e
Bhagyalakshmi217 <bhagyalakshmi2716@gmail.com>
2026-08-20 21:46:13 +0530

    Add Medium Java Solution from LeetCode

---
+++ LeetCode/Java/Medium/198. House Robber/Solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int rob(int[] nums) {
+        if(nums.length==1){
+            return nums[0];
+        }
+        int[] dp=new int[nums.length];
+        dp[0]=nums[0];
+        dp[1]=Math.max(nums[0],nums[1]);
+        for(int i=2;i<nums.length;i++){
+            int skip=dp[i-1];
+            int rob=nums[i]+dp[i-2];
+            dp[i]=Math.max(skip,rob);
+        }
+        return dp[nums.length-1];
+
+        
+    }
+}

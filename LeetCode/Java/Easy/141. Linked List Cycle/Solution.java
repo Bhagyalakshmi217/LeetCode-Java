0b62c4c38ebd84b9978aaea194984eb6221ae486
Bhagyalakshmi217 <bhagyalakshmi2716@gmail.com>
2026-08-21 13:43:26 +0530

    Add Easy Java Solution from LeetCode

--- LeetCode/Java/Easy/141. Linked List Cycle/Solution.java
+++ LeetCode/Java/Easy/141. Linked List Cycle/Solution.java
@@ -1,37 +1,5 @@
-/**
- * Definition for singly-linked list.
- * class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) {
- *         val = x;
- *         next = null;
- *     }
- * }
- */
-// public class Solution {
-//     public boolean hasCycle(ListNode head) {
-//         ListNode slow=head;
-//         ListNode fast=head;
-
-//         while(fast != null && fast.next!=null){
-//             slow=slow.next;
-//             fast=fast.next.next;
-
-//             if(slow==fast){
-//                 return true;
-//             }
-
-//         }
-//         return false;
-    
-
-        
-//     }
-// }
-
-class Solution{
-    public boolean hasCycle(ListNode head){
+public class Solution {
+    public boolean hasCycle(ListNode head) {
         ListNode slow=head;
         ListNode fast=head;
         while(fast!=null && fast.next!=null){
@@ -42,5 +10,6 @@ class Solution{
             }
         }
         return false;
+        
     }
 }

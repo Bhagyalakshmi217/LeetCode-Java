1229fb893b98beb251797e8bcb7848dca839a7f4
Bhagyalakshmi217 <bhagyalakshmi2716@gmail.com>
2026-08-23 12:15:30 +0530

    Add Java Solution to Patterns/Two Pointers

--- LeetCode/Patterns/Two Pointers/19. Remove Nth Node From End of List/Solution.java
+++ LeetCode/Patterns/Two Pointers/19. Remove Nth Node From End of List/Solution.java
@@ -1,20 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        ListNode fast=head;
-        ListNode slow=head;
-        for(int i=0;i<n;i++){
+        ListNode dummy=new ListNode(0);
+        dummy.next=head;
+        ListNode slow=dummy;
+        ListNode fast=dummy;
+        for(int i=0;i<=n;i++){
             fast=fast.next;
         }
-        if(fast==null){
-            return head.next;
-        }
-        while(fast.next!=null){           
-            fast=fast.next;
+        while(fast!=null){
             slow=slow.next;
+            fast=fast.next;
         }
-        ListNode dltNode=slow.next;
         slow.next=slow.next.next;
-        return head;
+        return dummy.next;
 
         
     }

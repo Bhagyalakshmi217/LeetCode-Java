a5a522e37e265c8da6197d064cd50f8601fac5d0
Bhagyalakshmi217 <bhagyalakshmi2716@gmail.com>
2026-08-23 10:24:15 +0530

    Add Java Solution to Patterns/Linked List

--- LeetCode/Patterns/Linked List/2. Add Two Numbers/Solution.java
+++ LeetCode/Patterns/Linked List/2. Add Two Numbers/Solution.java
@@ -1,33 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode dummyHead=new ListNode(0);
-        ListNode curr=dummyHead;
+        ListNode dummy=new ListNode(0);
+        ListNode current=dummy;
         int carry=0;
-        ListNode t1=l1;
-        ListNode t2=l2;
-        while(t1!=null || t2!=null){
-            int sum=carry;
-            if(t1!=null){
-                sum=sum+t1.val;
-            }
-            if(t2!=null){
-                sum=sum+t2.val;
-            }
-            ListNode newNode=new ListNode(sum%10);
+        while(l1!=null || l2!=null || carry!=0){
+            int val1=(l1!=null)?l1.val:0;
+            int val2=(l2!=null)?l2.val:0;
+
+            int sum=val1+val2+carry;
+            int digit=sum%10;
             carry=sum/10;
-            curr.next=newNode;
-            curr=curr.next;
-            if(t1!=null){
-                t1=t1.next;
+
+            current.next=new ListNode(digit);
+            current=current.next;
+            if(l1!=null){
+                l1=l1.next;
             }
-            if(t2!=null){
-                t2=t2.next;
-            }
-            if(carry!=0){
-                curr.next=new ListNode(carry);
+            if(l2!=null){
+                l2=l2.next;
             }
         }
-        return dummyHead.next;
+        return dummy.next;
+
         
     }
 }

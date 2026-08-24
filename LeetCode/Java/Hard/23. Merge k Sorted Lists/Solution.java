53a9e37518f32750debef872dc4d3a959ba10010
Bhagyalakshmi217 <bhagyalakshmi2716@gmail.com>
2026-08-24 10:01:16 +0530

    Add Hard Java Solution from LeetCode

--- LeetCode/Java/Hard/23. Merge k Sorted Lists/Solution.java
+++ LeetCode/Java/Hard/23. Merge k Sorted Lists/Solution.java
@@ -1,36 +1,29 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
         PriorityQueue<ListNode> pq=new PriorityQueue<>((a,b)->a.val-b.val);
-
+        // add first node of list to pq
         for(int i=0;i<lists.length;i++){
             if(lists[i]!=null){
-                pq.offer(lists[i]);
+                pq.add(lists[i]);
             }
         }
-
-        ListNode dummy=new ListNode(-1);
-        ListNode tail=dummy;
-
+        ListNode dummy=new ListNode(0);
+        ListNode temp=dummy;
         while(!pq.isEmpty()){
+            // get the smallest node
             ListNode node=pq.poll();
-            tail.next=node;
-            tail=node;
 
+            // add it to result
+            temp.next=node;
+            temp=temp.next;
+
+            // add next node from same list
             if(node.next!=null){
-                pq.offer(node.next);
-            }
+                pq.add(node.next);
+                            }
         }
         return dummy.next;
 
+        
     }
 }

c5b0fd101659412c3eab70599f550b239ca463f1
Bhagyalakshmi217 <bhagyalakshmi2716@gmail.com>
2026-08-25 18:49:04 +0530

    Add Java Solution to Patterns/Trees

--- LeetCode/Patterns/Trees/104. Maximum Depth of Binary Tree/Solution.java
+++ LeetCode/Patterns/Trees/104. Maximum Depth of Binary Tree/Solution.java
@@ -1,18 +1,3 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
     public int maxDepth(TreeNode root) {
         if(root==null){
@@ -21,7 +6,6 @@ class Solution {
         int left=maxDepth(root.left);
         int right=maxDepth(root.right);
         return 1+Math.max(left,right);
-
         
     }
 }

7b754595d9591a72720417819f28f5793f984ebc
Bhagyalakshmi217 <bhagyalakshmi2716@gmail.com>
2026-08-25 19:22:19 +0530

    Add Easy Java Solution from LeetCode

--- LeetCode/Java/Easy/110. Balanced Binary Tree/Solution.java
+++ LeetCode/Java/Easy/110. Balanced Binary Tree/Solution.java
@@ -1,21 +1,17 @@
 class Solution {
     public boolean isBalanced(TreeNode root) {
-        return height(root)!=-1;
+        return heightDfs(root)!=-1;
         
     }
-    private int height(TreeNode node){
-        if(node==null){
+    int heightDfs(TreeNode root){
+        if(root==null){
             return 0;
         }
-        int left=height(node.left);
-        int right=height(node.right);
-
-        if(left==-1 || right==-1){
-            return -1;
-        }
+        int left=heightDfs(root.left);
+        int right=heightDfs(root.right);
         if(Math.abs(left-right)>1){
             return -1;
         }
         return 1+Math.max(left,right);
-    }
+    } 
 }

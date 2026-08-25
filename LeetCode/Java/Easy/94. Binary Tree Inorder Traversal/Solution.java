6ab8b4b1502972f44f64233aa52b4fa95d3564a7
Bhagyalakshmi217 <bhagyalakshmi2716@gmail.com>
2026-08-25 11:18:08 +0530

    Add Easy Java Solution from LeetCode

--- LeetCode/Java/Easy/94. Binary Tree Inorder Traversal/Solution.java
+++ LeetCode/Java/Easy/94. Binary Tree Inorder Traversal/Solution.java
@@ -1,31 +1,18 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
+    List<Integer> result=new ArrayList<>();
     public List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer> ans=new ArrayList();
-        inorder(root,ans);
-        return ans;
         
+        inorder(root);
+        return result;
     }
-    private void inorder(TreeNode node,List<Integer>ans){
-        if(node==null){
+    private void inorder(TreeNode root){
+        if(root==null){
             return;
         }
-        inorder(node.left,ans);
-        ans.add(node.val);
-        inorder(node.right,ans);
+        
+        inorder(root.left);
+        result.add(root.val);
+        inorder(root.right);
+        
     }
 }

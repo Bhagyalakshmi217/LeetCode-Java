45dc6abbc080dee1f63cff94f09e0c33fd4ca2a5
Bhagyalakshmi217 <bhagyalakshmi2716@gmail.com>
2026-08-26 19:45:13 +0530

    Add Hard Java Solution for 1029. Vertical Order Traversal of a Binary Tree from LeetCode

--- LeetCode/Java/Hard/1029. Vertical Order Traversal of a Binary Tree/README.md
+++ LeetCode/Java/Hard/1029. Vertical Order Traversal of a Binary Tree/README.md
@@ -8,8 +8,8 @@
 Hash Table, Tree, Depth-First Search, Breadth-First Search, Sorting, Binary Tree
 
 ### 🚀 Performance
-- **Runtime:** N/A
-- **Memory:** N/A
+- **Runtime:** 4 ms
+- **Memory:** 44.1 MB
 
 ---
 
--- LeetCode/Java/Hard/1029. Vertical Order Traversal of a Binary Tree/Solution.java
+++ LeetCode/Java/Hard/1029. Vertical Order Traversal of a Binary Tree/Solution.java
@@ -1,142 +1,76 @@
-// class Solution {
-
-//     class Pair {
-//         TreeNode node;
-//         int row;
-//         int col;
-
-//         Pair(TreeNode node, int row, int col) {
-//             this.node = node;
-//             this.row = row;
-//             this.col = col;
-//         }
-//     }
-
-//     public List<List<Integer>> verticalTraversal(TreeNode root) {
-
-//         List<List<Integer>> ans = new ArrayList<>();
-
-//         if (root == null) {
-//             return ans;
-//         }
-
-//         // column -> list of (row, value)
-//         TreeMap<Integer, List<int[]>> map = new TreeMap<>();
-
-//         Queue<Pair> q = new LinkedList<>();
-
-//         // root starts at row 0, column 0
-//         q.offer(new Pair(root, 0, 0));
-
-//         while (!q.isEmpty()) {
-
-//             Pair current = q.poll();
-
-//             TreeNode node = current.node;
-//             int row = current.row;
-//             int col = current.col;
-
-//             map.putIfAbsent(col, new ArrayList<>());
-
-//             // store row and value
-//             map.get(col).add(new int[]{row, node.val});
-
-//             // left child
-//             if (node.left != null) {
-//                 q.offer(new Pair(node.left, row + 1, col - 1));
-//             }
-
-//             // right child
-//             if (node.right != null) {
-//                 q.offer(new Pair(node.right, row + 1, col + 1));
-//             }
-//         }
-
-//         // Process columns from left to right
-//         for (List<int[]> list : map.values()) {
-
-//             // Sort by:
-//             // 1. row
-//             // 2. value if same row
-//             Collections.sort(list, (a, b) -> {
-
-//                 if (a[0] != b[0]) {
-//                     return a[0] - b[0];
-//                 }
-
-//                 return a[1] - b[1];
-//             });
-
-//             List<Integer> column = new ArrayList<>();
-
-//             for (int[] pair : list) {
-//                 column.add(pair[1]);
-//             }
-
-//             ans.add(column);
-//         }
-
-//         return ans;
-//     }
-// }
-
-class Tuple {
-    TreeNode node;
-    int row;
-    int col;
-
-    public Tuple(TreeNode node, int row, int col) {
-        this.node = node;
-        this.row = row;
-        this.col = col;
-    }
-}
-
 class Solution {
+
+    class Pair {
+        TreeNode node;
+        int row;
+        int col;
+
+        Pair(TreeNode node, int row, int col) {
+            this.node = node;
+            this.row = row;
+            this.col = col;
+        }
+    }
+
     public List<List<Integer>> verticalTraversal(TreeNode root) {
 
         List<List<Integer>> ans = new ArrayList<>();
 
-        TreeMap<Integer, PriorityQueue<Integer>> map = new TreeMap<>();
+        if (root == null) {
+            return ans;
+        }
 
-        Queue<Tuple> q = new LinkedList<>();
+        // column -> list of (row, value)
+        TreeMap<Integer, List<int[]>> map = new TreeMap<>();
 
-        q.offer(new Tuple(root, 0, 0));
+        Queue<Pair> q = new LinkedList<>();
+
+        // root starts at row 0, column 0
+        q.offer(new Pair(root, 0, 0));
 
         while (!q.isEmpty()) {
 
-            Tuple tuple = q.poll();
+            Pair current = q.poll();
 
-            TreeNode node = tuple.node;
-            int row = tuple.row;
-            int col = tuple.col;
+            TreeNode node = current.node;
+            int row = current.row;
+            int col = current.col;
 
-            // If column doesn't exist
-            if (!map.containsKey(col)) {
-                map.put(col, new PriorityQueue<>());
-            }
+            map.putIfAbsent(col, new ArrayList<>());
 
-            // Add node value to that column
-            map.get(col).offer(node.val);
+            // store row and value
+            map.get(col).add(new int[]{row, node.val});
 
-            // Left child
+            // left child
             if (node.left != null) {
-                q.offer(new Tuple(node.left, row + 1, col - 1));
+                q.offer(new Pair(node.left, row + 1, col - 1));
             }
 
-            // Right child
+            // right child
             if (node.right != null) {
-                q.offer(new Tuple(node.right, row + 1, col + 1));
+                q.offer(new Pair(node.right, row + 1, col + 1));
             }
         }
 
-        // TreeMap gives columns from left to right
-        for (PriorityQueue<Integer> pq : map.values()) {
+        // Process columns from left to right
+        for (List<int[]> list : map.values()) {
+
+            // Sort by:
+            // 1. row
+            // 2. value if same row
+            Collections.sort(list, (a, b) -> {
+
+                if (a[0] != b[0]) {
+                    return a[0] - b[0];
+                }
+
+                return a[1] - b[1];
+            });
 
             List<Integer> column = new ArrayList<>();
 
-            while (!pq.isEmpty()) {
-                column.add(pq.poll());
+            for (int[] pair : list) {
+                column.add(pair[1]);
             }
 
             ans.add(column);

e41efe16d0e6f2fbe0d7d3351a201feec50fd51b
Bhagyalakshmi217 <bhagyalakshmi2716@gmail.com>
2026-09-02 17:01:18 +0530

    Add Easy Java Solution for 94. Binary Tree Inorder Traversal from LeetCode

--- LeetCode/Patterns/Trees/94. Binary Tree Inorder Traversal/Solution.java
+++ LeetCode/Patterns/Trees/94. Binary Tree Inorder Traversal/Solution.java
@@ -1,18 +1,70 @@
+// class Solution {
+//     List<Integer> result=new ArrayList<>();
+//     public List<Integer> inorderTraversal(TreeNode root) {
+        
+//         inorder(root);
+//         return result;
+//     }
+//     private void inorder(TreeNode root){
+//         if(root==null){
+//             return;
+//         }
+        
+//         inorder(root.left);
+//         result.add(root.val);
+//         inorder(root.right);
+        
+//     }
+// }
+
 class Solution {
-    List<Integer> result=new ArrayList<>();
     public List<Integer> inorderTraversal(TreeNode root) {
-        
-        inorder(root);
-        return result;
-    }
-    private void inorder(TreeNode root){
-        if(root==null){
-            return;
+
+        List<Integer> ans = new ArrayList<>();
+
+        TreeNode curr = root;
+
+        while (curr != null) {
+
+            // Case 1: No left subtree
+            if (curr.left == null) {
+
+                ans.add(curr.val);
+                curr = curr.right;
+            }
+
+            // Case 2: Left subtree exists
+            else {
+
+                // Find inorder predecessor
+                TreeNode prev = curr.left;
+
+                while (prev.right != null && prev.right != curr) {
+                    prev = prev.right;
+                }
+
+                // First time visiting curr
+                if (prev.right == null) {
+
+                    // Create thread
+                    prev.right = curr;
+
+                    curr = curr.left;
+                }
+
+                // Thread already exists
+                else {
+
+                    // Remove thread
+                    prev.right = null;
+
+                    ans.add(curr.val);
+
+                    curr = curr.right;
+                }
+            }
         }
-        
-        inorder(root.left);
-        result.add(root.val);
-        inorder(root.right);
-        
+
+        return ans;
     }
 }

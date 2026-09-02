4316a12fdb40d5e879fc8c5170ec8b5630955a75
Bhagyalakshmi217 <bhagyalakshmi2716@gmail.com>
2026-09-02 17:04:15 +0530

    Add Easy Java Solution for 144. Binary Tree Preorder Traversal from LeetCode

--- LeetCode/Patterns/Trees/144. Binary Tree Preorder Traversal/Solution.java
+++ LeetCode/Patterns/Trees/144. Binary Tree Preorder Traversal/Solution.java
@@ -1,16 +1,66 @@
-class Solution {
-    List<Integer> result=new ArrayList<>();
-    public List<Integer> preorderTraversal(TreeNode root) {
-        preorder(root);
-        return result;
+// class Solution {
+//     List<Integer> result=new ArrayList<>();
+//     public List<Integer> preorderTraversal(TreeNode root) {
+//         preorder(root);
+//         return result;
         
-    }
-    private void preorder(TreeNode root){
-        if(root==null){
-            return;
+//     }
+//     private void preorder(TreeNode root){
+//         if(root==null){
+//             return;
+//         }
+//         result.add(root.val);
+//         preorder(root.left);
+//         preorder(root.right);
+//     }
+// }
+
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+
+        List<Integer> ans = new ArrayList<>();
+
+        TreeNode curr = root;
+
+        while (curr != null) {
+
+            // Case 1: No left subtree
+            if (curr.left == null) {
+
+                ans.add(curr.val);
+                curr = curr.right;
+            }
+
+            // Case 2: Left subtree exists
+            else {
+
+                // Find inorder predecessor
+                TreeNode prev = curr.left;
+
+                while (prev.right != null && prev.right != curr) {
+                    prev = prev.right;
+                }
+
+                // First time visiting curr
+                if (prev.right == null) {
+                    ans.add(curr.val);
+
+                    // Create thread
+                    prev.right = curr;
+
+                    curr = curr.left;
+                }
+
+                // Thread already exists
+                else {
+
+                    // Remove thread
+                    prev.right = null;
+                    curr = curr.right;
+                }
+            }
         }
-        result.add(root.val);
-        preorder(root.left);
-        preorder(root.right);
+
+        return ans;
     }
 }

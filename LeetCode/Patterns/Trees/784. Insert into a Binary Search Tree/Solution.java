a5a9e786bf177d2283211f1d0da2a86a1e71f428
Bhagyalakshmi217 <bhagyalakshmi2716@gmail.com>
2026-09-03 21:27:58 +0530

    Add Medium Java Solution for 784. Insert into a Binary Search Tree from LeetCode

--- LeetCode/Patterns/Trees/784. Insert into a Binary Search Tree/Solution.java
+++ LeetCode/Patterns/Trees/784. Insert into a Binary Search Tree/Solution.java
@@ -1,14 +1,42 @@
+// class Solution {
+//     public TreeNode insertIntoBST(TreeNode root, int val) {
+//         if(root==null){
+//             return new TreeNode(val);
+//         }
+//         if(val<root.val){
+//             root.left=insertIntoBST(root.left,val);
+//         }else{
+//             root.right=insertIntoBST(root.right,val);
+//         }
+//         return root;
+        
+//     }
+// }
+
+
 class Solution {
     public TreeNode insertIntoBST(TreeNode root, int val) {
         if(root==null){
             return new TreeNode(val);
         }
-        if(val<root.val){
-            root.left=insertIntoBST(root.left,val);
-        }else{
-            root.right=insertIntoBST(root.right,val);
+        TreeNode curr=root;
+        while(true){
+            if(curr.val>val){
+                if(curr.left!=null){
+                    curr=curr.left;
+                }else{
+                    curr.left=new TreeNode(val);
+                    break;
+                }
+            }else{
+                if(curr.right!=null){
+                    curr=curr.right;
+                }else{
+                    curr.right= new TreeNode(val);
+                    break;
+            }
+        }
         }
         return root;
-        
     }
 }

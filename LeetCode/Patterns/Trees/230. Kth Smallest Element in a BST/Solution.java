249f0b93455dc810b6d0ff7b0f9582cd6f1afac8
Bhagyalakshmi217 <bhagyalakshmi2716@gmail.com>
2026-09-04 09:50:48 +0530

    Add Medium Java Solution for 230. Kth Smallest Element in a BST from LeetCode

--- LeetCode/Patterns/Trees/230. Kth Smallest Element in a BST/Solution.java
+++ LeetCode/Patterns/Trees/230. Kth Smallest Element in a BST/Solution.java
@@ -1,25 +1,53 @@
+// class Solution {
+
+//     int count = 0;
+
+//     public int kthSmallest(TreeNode root, int k) {
+
+//         if (root == null) {
+//             return -1;
+//         }
+
+//         int left = kthSmallest(root.left, k);
+
+//         if (left != -1) {
+//             return left;
+//         }
+
+//         count++;
+
+//         if (count == k) {
+//             return root.val;
+//         }
+
+//         return kthSmallest(root.right, k);
+//     }
+// }
+
 class Solution {
-
-    int count = 0;
-
     public int kthSmallest(TreeNode root, int k) {
 
-        if (root == null) {
-            return -1;
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode curr = root;
+
+        while (curr != null || !stack.isEmpty()) {
+
+            while (curr != null) {
+                stack.push(curr);
+                curr = curr.left;
+            }
+
+            curr = stack.pop();
+
+            k--;
+
+            if (k == 0) {
+                return curr.val;
+            }
+
+            curr = curr.right;
         }
 
-        int left = kthSmallest(root.left, k);
-
-        if (left != -1) {
-            return left;
-        }
-
-        count++;
-
-        if (count == k) {
-            return root.val;
-        }
-
-        return kthSmallest(root.right, k);
+        return -1;
     }
 }

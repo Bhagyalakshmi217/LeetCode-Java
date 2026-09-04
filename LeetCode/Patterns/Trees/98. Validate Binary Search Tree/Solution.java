43cbf65c0843ef861324624f65c0ead1ba53cd9d
Bhagyalakshmi217 <bhagyalakshmi2716@gmail.com>
2026-09-04 10:23:44 +0530

    Add Medium Java Solution for 98. Validate Binary Search Tree from LeetCode

--- LeetCode/Patterns/Trees/98. Validate Binary Search Tree/Solution.java
+++ LeetCode/Patterns/Trees/98. Validate Binary Search Tree/Solution.java
@@ -1,33 +1,17 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
     public boolean isValidBST(TreeNode root) {
-        return validate(root,Long.MIN_VALUE,Long.MAX_VALUE);
+        return isValidBST(root,Long.MIN_VALUE,Long.MAX_VALUE);
         
     }
-    private boolean validate(TreeNode node,long min,long max){
-        if(node==null){
+    public boolean isValidBST(TreeNode root,long min,long max){
+        if(root==null){
             return true;
         }
-        if(node.val<=min || node.val>=max){
+        if(root.val<=min || root.val>=max){
             return false;
         }
-        boolean left=validate(node.left,min,node.val);
-        boolean right=validate(node.right,node.val,max);
-    
-    return left && right;
-}
+        return isValidBST(root.left,min,root.val)
+            && isValidBST(root.right,root.val,max);
+
+    }
 }

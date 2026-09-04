08d90d91e821d035803890312a521c7904874dd0
Bhagyalakshmi217 <bhagyalakshmi2716@gmail.com>
2026-09-04 14:13:43 +0530

    Add Medium Java Solution for 173. Binary Search Tree Iterator from LeetCode

---
+++ LeetCode/Patterns/Trees/173. Binary Search Tree Iterator/Solution.java
@@ -0,0 +1,27 @@
+class BSTIterator {
+    Stack<TreeNode> stack=new Stack<>();
+
+    public BSTIterator(TreeNode root) {
+        pushLeft(root);
+        
+    }
+    
+    public int next() {
+        TreeNode node=stack.pop();
+        pushLeft(node.right);
+        return node.val;
+        
+    }
+    
+    public boolean hasNext() {
+        return (!stack.isEmpty());
+        
+    }
+    private void pushLeft(TreeNode root){
+        while(root!=null){
+            stack.push(root);
+            root=root.left;
+        }
+    } 
+}
+

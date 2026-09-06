1f5fb32c21b503efee012adab9e631c3b0ee2a71
Bhagyalakshmi217 <bhagyalakshmi2716@gmail.com>
2026-09-06 16:22:44 +0530

    Add Medium Java Solution for 167. Two Sum II - Input Array Is Sorted from LeetCode

--- LeetCode/Java/Medium/167. Two Sum II - Input Array Is Sorted/Solution.java
+++ LeetCode/Java/Medium/167. Two Sum II - Input Array Is Sorted/Solution.java
@@ -4,19 +4,17 @@ class Solution {
         int right=numbers.length-1;
         while(left<right){
             int sum=numbers[left]+numbers[right];
-            if(sum<target){
-                left++;
-            }
-            else if(sum>target){
+            if(sum>target){
                 right--;
+            }
+            else if(sum<target){
+                left++;
             }
             else{
                 return new int[]{left+1,right+1};
-
             }
         }
         return new int[]{-1,-1};
+        
     }
 }
-        
-    

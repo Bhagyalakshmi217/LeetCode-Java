4ac62769d58914ea5b6fe0c3ae9294a945df712a
Bhagyalakshmi217 <bhagyalakshmi2716@gmail.com>
2026-09-08 23:57:05 +0530

    Add Easy Java Solution for 4245. Count Commas in Range from LeetCode

--- LeetCode/Java/Easy/4245. Count Commas in Range/Solution.java
+++ LeetCode/Java/Easy/4245. Count Commas in Range/Solution.java
@@ -1,13 +1,6 @@
 class Solution {
     public int countCommas(int n) {
-        int count=0;
-        if(n>=1000){
-            count+=n-999;
-        }
-        if(n>=10000){
-            count+=n-9999;
-        }
-        return count;
+        return Math.max(0,n-999);
         
     }
 }

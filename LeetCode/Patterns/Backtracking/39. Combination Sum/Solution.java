9e7888843be646da3c573410a4658a0a1a592707
Bhagyalakshmi217 <bhagyalakshmi2716@gmail.com>
2026-09-11 21:40:11 +0530

    Add Medium Java Solution for 39. Combination Sum from LeetCode

--- LeetCode/Patterns/Backtracking/39. Combination Sum/Solution.java
+++ LeetCode/Patterns/Backtracking/39. Combination Sum/Solution.java
@@ -6,17 +6,21 @@ class Solution {
         
     }
     private void findCombination(int ind,int[] arr,int target,List<List<Integer>> ans,List<Integer> ds){
+        // base case
         if(ind==arr.length){
             if(target==0){
                 ans.add(new ArrayList<>(ds));
             }
             return;
         }
+        // pick the current element
         if(arr[ind]<=target){
             ds.add(arr[ind]);
             findCombination(ind,arr,target-arr[ind],ans,ds);
+            // backtrack
             ds.remove(ds.size()-1);
         }
+        // not pick the current element
         findCombination(ind+1,arr,target,ans,ds);
     }
 }
